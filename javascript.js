03534b34e9780800ac7cb2756c1151c970f8b0fd
suryanto23 <sur23.id@gmail.com>
2021-01-22 13:00:22 +0700

    upload file-file utama

---
+++ javascript.js
@@ -0,0 +1,49 @@
+
+
+// Soal nomor 1
+//Write nama 
+console.log("Nama : Suryanto");
+//Write daerah asal
+console.log("Daerah Asal : Batam");
+//Write tanggal lahir
+console.log("Tanggal Lahir : 23 Januari 1997");
+//Write umur
+console.log("Umur : 23 Tahun");
+//Write pekerjaan
+console.log("Posisi Pekerjaan : Tidak Bekerja");
+
+// Soal nommor 2
+/*Membuat prompt nama user 
+dan asal negara*/
+let nama = prompt("Masukkan nama anda:");
+console.log("Nama user adalah " +nama);
+let asal = prompt("Masukkan asal negara anda:");
+console.log("Asal negara dari user " +nama +" adalah " +asal);
+confirm("Apakah data yang diisi sudah benar?");
+
+// Soal nomor 3 (sudah diberikan single comment di soal nomor 1)
+// Soal nomor 4 (sudah diberikan double commment di soal nomor 2)
+
+// Soal nomor 5
+let user = {
+    Nama : "Suryanto",
+    daerahAsal : "Batam",
+    tanggalLahir : "23 Januari 1997",
+    Umur : 23,
+    Pekerjaan : "Tidak Bekerja"
+};
+
+// Soal nomor 6
+let tambah = 25 + 10;
+console.log(tambah);
+
+let kali = 100 * 2;
+console.log(kali);
+
+let bagi = 99 / 2;
+console.log(bagi);
+
+let modulus = 99 % 2;
+console.log(modulus);
+
+console.log(user);
